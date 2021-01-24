87bdfad1d5fb952a76a42716f89e9f47deabfde2
tunchunairarko <tunchunairarko@gmail.com>
2021-01-24 13:57:45 +0100

    made changes

--- btz-backend/index.js
+++ btz-backend/index.js
@@ -9,6 +9,7 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
+
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`The server has started on port: ${PORT}`));
