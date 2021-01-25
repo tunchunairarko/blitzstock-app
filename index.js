5819d55e4c3004b36a30eb19172c8926c037dfbc
tunchunairarko <tunchunairarko@gmail.com>
2021-01-25 02:35:47 +0100

    c

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ app.use(express.json());
 app.use(cors());
 // app.use(express.static("../btz-front/build"))
 app.use(helmet());
-app.use(serveStatic("client/public"))
+app.use(serveStatic("client"))
 
 const PORT = process.env.PORT || 4000;
 
