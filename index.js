e9951bc06c4c983078e086410ff4c8501541d87f
tunchunairarko <tunchunairarko@gmail.com>
2021-01-30 17:21:09 +0100

    asd

--- index.js
+++ index.js
@@ -11,11 +11,11 @@ app.use(cors());
 // app.use(express.static("client/build"))
 
 const root = require('path').join(__dirname, 'client', 'build')
-// app.use(express.static(root));
+app.use(express.static(root));
 
-app.use("*", function (req, res) {
-  res.sendFile('index.html', { root });
-})
+// app.use("*", function (req, res) {
+//   res.sendFile('index.html', { root });
+// })
 
 app.all('/api', function (req, res, next) {
   console.log('Accessing the secret section ...')
