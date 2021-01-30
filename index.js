306a6b17f719b7e1da6495a38ab5d7f39736e5ea
tunchunairarko <tunchunairarko@gmail.com>
2021-01-30 16:21:45 +0100

    iop

--- index.js
+++ index.js
@@ -15,12 +15,14 @@ app.use(express.static(root));
 
 
 
+
+app.get("*", function (req, res) {
+  res.sendFile('index.html', { root });
+})
+
 app.all('/api', function (req, res, next) {
   console.log('Accessing the secret section ...')
   next() // pass control to the next handler
-})
-app.get("/\/((?!api).)*/", function (req, res) {
-  res.sendFile('index.html', { root });
 })
 
 app.use(helmet());
