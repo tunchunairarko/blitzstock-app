6e7f5f2ec5d8050fec9eda69c8350b39a988fee4
tunchunairarko <tunchunairarko@gmail.com>
2021-01-30 15:43:31 +0100

    f

--- index.js
+++ index.js
@@ -13,13 +13,13 @@ app.use(cors());
 const root = require('path').join(__dirname, 'client', 'build')
 app.use(express.static(root));
 
+
+app.get("/", (req, res) => {
+    res.sendFile('index.html', { root });
+})
 app.use('/api', function (req, res, next) {
   next();
 });
-
-app.get("*", (req, res) => {
-    res.sendFile('index.html', { root });
-})
 
 app.use(helmet());
 
