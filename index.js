c1d0438cc1427760505c3251b20370db74424b95
tunchunairarko <tunchunairarko@gmail.com>
2021-01-30 15:55:12 +0100

    k

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ app.all('/api', function (req, res, next) {
   console.log('Accessing the secret section ...')
   next() // pass control to the next handler
 })
-app.get("*", (req, res) => {
+app.get("/", function (req, res) {
   res.sendFile('index.html', { root });
 })
 
