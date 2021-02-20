c4d0822385465ef21726694d2ee71ba642b5a1b6
tunchunairarko <tunchunairarko@gmail.com>
2021-02-20 17:23:24 +0100

    Barcode fixing going on

--- client/src/components/scanner/Scanner.js
+++ client/src/components/scanner/Scanner.js
@@ -2,54 +2,96 @@ import React, { useEffect, useState, Fragment } from 'react'
 import Quagga from 'quagga'
 import { Modal } from 'react-bootstrap'
 import "../../components/assets/style.css"
-
+import useWindowDimensions from './useWindowDimensions';
+import {
+    isMobile
+  } from "react-device-detect";
 export default function Scanner({ show, onDetected, handleScanner }) {
+    const { height, width } = useWindowDimensions();
     useEffect(() => {
-        Quagga.init(
-            {
-                inputStream: {
-                    type: 'LiveStream',
-                    constraints: {
-                          width: 250,
-                          height: 200,
-                        facingMode: 'environment', // or user
+        if(isMobile){
+            Quagga.init(
+                {
+                    inputStream: {
+                        type: 'LiveStream',
+                        constraints: {
+                            width: width*3/4,
+                            height:height*3/4,
+                            facingMode: 'environment', // or user
+                        },
                     },
+                    locator: {
+                        patchSize: 'medium',
+                        halfSample: true,
+                    },
+                    numOfWorkers: 4,
+                    decoder: {
+                        readers: ['code_128_reader', 'ean_reader', 'upc_reader','ean_8_reader','code_39_reader','codabar_reader','code_93_reader'],
+                    },
+                    locate: true,
                 },
-                locator: {
-                    patchSize: 'medium',
-                    halfSample: true,
+                function (err) {
+                    if (err) {
+                        return console.log(err)
+                    }
+                    Quagga.start()
                 },
-                numOfWorkers: 4,
-                decoder: {
-                    readers: ['code_128_reader', 'ean_reader', 'upc_reader','ean_8_reader','code_39_reader','codabar_reader','code_93_reader'],
+            )
+            Quagga.onDetected(handleDetected)
+            return function cleanup() {
+                Quagga.offDetected(onDetected)
+                Quagga.stop()
+            }
+        }
+        else{
+            Quagga.init(
+                {
+                    inputStream: {
+                        type: 'LiveStream',
+                        constraints: {
+                            width: width/2,
+                            height:height/2,
+                            facingMode: 'environment', // or user
+                        },
+                    },
+                    locator: {
+                        patchSize: 'medium',
+                        halfSample: true,
+                    },
+                    numOfWorkers: 4,
+                    decoder: {
+                        readers: ['code_128_reader', 'ean_reader', 'upc_reader','ean_8_reader','code_39_reader','codabar_reader','code_93_reader'],
+                    },
+                    locate: true,
                 },
-                locate: true,
-            },
-            function (err) {
-                if (err) {
-                    return console.log(err)
-                }
-                Quagga.start()
-            },
-        )
-        Quagga.onDetected(handleDetected)
-        return function cleanup() {
-            Quagga.offDetected(onDetected)
-            Quagga.stop()
+                function (err) {
+                    if (err) {
+                        return console.log(err)
+                    }
+                    Quagga.start()
+                },
+            )
+            Quagga.onDetected(handleDetected)
+            return function cleanup() {
+                Quagga.offDetected(onDetected)
+                Quagga.stop()
+            }
         }
     }, [])
     const handleDetected = (result) => {
         onDetected(result)
     }
     return (
-        <Modal size="lg" show={show} onHide={(e)=>handleScanner(false)} backdrop="static" keyboard={false} centered>
+        <Modal size="lg"  show={show} onHide={(e)=>handleScanner(false)} backdrop="static" keyboard={false} centered>
             <Modal.Header closeButton>
                 <Modal.Title id="searchResTitle">Scan barcode</Modal.Title>
             </Modal.Header>
-            <Modal.Body>
-                <div id="interactive" className="viewport">
+            <Modal.Body style={{width:width/2, height:height*3/5.5}}>
+                <center>
+                    <div id="interactive" className="viewport" style={{margin:auto, width:width/2, height:height*3/5.5}}>
 
-                </div>
+                    </div>
+                </center>
             </Modal.Body>
 
         </Modal>
